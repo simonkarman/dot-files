b15588e43b9017b66c73538cb969bda42630dede
Simon Karman <simonkarman@binx.io>
2021-10-19 14:03:21 +0200

    Added most important dot files as symlinks

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
+ROOT="/Users/simonkarman/projects/simonkarman/dot-files"
 echo "Setting up .gitconfig"
-ln -s .globalgitconfig ~/.gitconfig
-ln -s .globalgitignore ~/.globalgitignore
-ln -s .zshrc ~/.zshrc
+ln -s "$ROOT/.globalgitconfig" ~/.gitconfig
+ln -s "$ROOT/.globalgitignore" ~/.globalgitignore
+ln -s "$ROOT/.zshrc" ~/.zshrc
